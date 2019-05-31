1d9687c43245f8dfaa4f5821b6797d33eea518ae
Jay <michal.sidor3@pollub.edu.pl>
2019-05-31 14:56:19 +0200

    Add prompts for granting required premissions

--- app/src/main/java/com/example/komunikacja_sieciowa/MainActivity.java
+++ app/src/main/java/com/example/komunikacja_sieciowa/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.komunikacja_sieciowa;
 
+import android.Manifest;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +12,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private String www;
+    private int MY_PERMISSIONS_REQUEST_INTERNET = 47, MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 48;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +20,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         addListeners();
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                MY_PERMISSIONS_REQUEST_INTERNET);
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.INTERNET},
+                MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+
 
     }
 
